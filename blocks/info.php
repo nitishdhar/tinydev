554ad3ffd5c2146b2cd0be89f235dac49bc96518
Nitish Dhar <nitish@chanantechnologies.com>
2012-06-15 21:09:00 +0530

    Added sample block

---
+++ blocks/info.php
@@ -0,0 +1,6 @@
+<!-- 
+Suppose to be used when you have a
+big chunk of code which you need to put in 
+the website at multiple places & is probably going to change 
+-->
+<img src="http://placehold.it/270X72&text=Info Block" alt="Logo" title="<?php echo SITE_NAME; ?>"/>
--- global/header.php
+++ global/header.php
@@ -1,7 +1,14 @@
 <div class="header">
     <div class="container">
-            <img src="http://placehold.it/300X80&text=Awesome Logo" alt="Logo" title="<?php echo SITE_NAME; ?>"/>
-            <br/><br/>
+        <div class="row">
+            <div class="span4">
+                <img src="http://placehold.it/300X80&text=Awesome Logo" alt="Logo" title="<?php echo SITE_NAME; ?>"/>
+            <br/><br/>  
+            </div>
+            <div class="span3 offset5">
+                <?php include_once 'blocks/info.php'; ?>
+            </div>
+        </div>
     </div>
 </div>
 <?php include_once 'main_menu.php'; ?>
