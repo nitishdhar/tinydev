e028f3ae778198f08c5ef67f65940ddb7d49191e
Nitish Dhar <nitish@chanantechnologies.com>
2012-06-16 05:23:31 +0530

    Added custom 404 page setup

---
+++ 404.php
@@ -0,0 +1,27 @@
+<?php include_once 'global/super_head.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <?php include_once 'blocks/head.php'; ?>
+    </head>
+    <body>
+        <div class="container">
+            <!-- Header -->
+            <?php include_once 'blocks/header.php'; ?>
+            <!-- Header Ends-->
+            <!-- This page content -->
+            <div class="row">
+                <div class="span12">
+                    <h2>Oh Snap! We cannot find what you are looking for... </h2>
+                </div>
+            </div>
+
+            <!-- This page content Ends Here -->
+
+            <!-- Footer Here -->
+            <?php include_once 'blocks/footer.php'; ?>
+            <!-- Footer Ends Here -->
+
+        </div> <!-- /container -->
+    </body>
+</html>
